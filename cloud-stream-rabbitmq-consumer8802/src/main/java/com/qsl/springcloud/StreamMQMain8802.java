b1c69d8e36e11de6ac92c32763a2ba21ddc917fa
qslGitHub <740237940@qq.com>
2021-07-24 22:03:24 +0800

    提交消息驱动，消息消费者服务代码

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/qsl/springcloud/StreamMQMain8802.java
@@ -0,0 +1,15 @@
+package com.qsl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author sileiqiang
+ * @create 2021/7/23 10:50 下午
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class,args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/qsl/springcloud/StreamMQMain8803.java
@@ -0,0 +1,16 @@
+package com.qsl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author sileiqiang
+ * @create 2021/7/23 10:34 下午
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
