ddcf32efa2384d59743872c16828d3b42eb5a2d0
qslGitHub <740237940@qq.com>
2021-08-01 23:16:21 +0800

    提交spring cloud alibaba nacos服务配置中心代码

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/qsl/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.qsl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author sileiqiang
+ * @create 2021/8/1 10:40 下午
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
